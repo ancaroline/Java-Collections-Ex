8491618cc16aa2ea445f5ad740d0ab89343bbcb0
Anne Caroline <carolaragao94@gmail.com>
2022-07-18 17:11:10 -0300

    Desafios list

---
+++ src/collectionsList/DesafiosList.java
@@ -0,0 +1,14 @@
+package collectionsList;
+
+public class DesafiosList {
+    public static void main(String[] args) {
+        /*
+        Resolva esses exercícios utilizando os métodos da implementação LinkedList:
+        System.out.println("Crie uma lista chamada notas2: " + "coloque todos os elementos
+        da List ArrayList nessa nova lista:);
+
+        System.out.println("Mostre a primeira nota da nova lista sem removê-lo");
+        System.out.println("Mostre a primeira nota da nova lista removendo-o");
+         */
+    }
+}
