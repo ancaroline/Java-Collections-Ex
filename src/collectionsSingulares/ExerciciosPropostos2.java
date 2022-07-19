6f97e47f859d731de7067e1c9cabf8edac172f64
Anne Caroline <carolaragao94@gmail.com>
2022-07-19 17:19:41 -0300

    Exercícios Propostos Set

---
+++ src/collectionsSingulares/ExerciciosPropostos2.java
@@ -0,0 +1,17 @@
+package collectionsSingulares;
+/*
+Crie uma class LinguagemFavorita que possua os atributos nome, anoDeCriação e ide.
+Em seguida, crie um conjunto com 3 linguagens e faça um programa que ordene esse conjunto por:
+Ordem de inserção
+Ordem natual
+IDE
+Ano de criação e nome
+Nome, ano de criação e IDE
+Ao final, exiba as linguagens no console, um abaixo da outra
+ */
+
+public class ExerciciosPropostos2 {
+    public static void main(String[] args) {
+
+    }
+}
