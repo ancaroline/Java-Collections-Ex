2c9a96fc2ea985a65d5dced15e86adade8b52715
Anne Caroline <carolaragao94@gmail.com>
2022-07-19 11:44:41 -0300

    Exercícios propostos List

---
+++ src/collectionsList/ExercicioPropostoList1.java
@@ -0,0 +1,11 @@
+package collectionsList;
+/*
+Faça um programa que receba a temperatura média dos 6 primeiros meses do ano e armazene-as em uma lista.
+Após, calcule a média semestral das temps e mostre as temps acima da média, e em que mês elas
+ocorreram(mostrar o mês por extenso: 1-Janeiro, 1-Fevereiro)
+ */
+public class ExercicioPropostoList1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/collectionsList/ExercicioPropostoList2.java
@@ -0,0 +1,19 @@
+package collectionsList;
+/*
+Utilizando listas, faça um programa que faça 5 perguntas para uma pessoa sobre um crime.
+1. telefonou para a vítima?
+2. esteve no local do crime?
+3. mora perto da vítima?
+4. devia para a vítima?
+5. já trabalhou com a vítima?
+
+Respostas: s/n
+
+Se a pessoa responder positivamente as 2 questoes, ela deve ser classificada como suspeita, entre
+3 e 4, cúmplice e 5 como assassina. Caso contrário, inocente.
+ */
+public class ExercicioPropostoList2 {
+    public static void main(String[] args) {
+
+    }
+}
