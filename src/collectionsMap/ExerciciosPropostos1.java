397d1589125c155b5a5ff082be0bf64ac1753bfd
Anne Caroline <carolaragao94@gmail.com>
2022-07-20 10:16:41 -0300

    Desafio e Exercício Proposto

---
+++ src/collectionsMap/ExerciciosPropostos1.java
@@ -0,0 +1,28 @@
+package collectionsMap;
+/*
+Dada a população estimada de alguns estados do NE brasileiro, faça:
+Estado = PE - População = 9.616.621
+Estado = Al - População = 3.351.543
+Estado = CE - População = 9.187.103
+Estado = RN - População = 3.534.265
+
+Crie um dicionário e relacione os estados e as suas populações
+Substitua a população do estado do RN por 3.534.165
+Confira se o estado PB está no dicionário, caso não, adicione: PB - 4.039.277
+Exiba a população PE
+Exiba todos os estados e as suas populações na ordem em que foi informado
+Exiba os estados e as suas populações em ordem alfabética;
+Exiba o estado com o menor população e a sua quantidade;
+Exiba o estado com a maior população e a sua quantidade;
+Exiba a soma da população desses estados;
+Exiba a média da população deste dicionário de estados;
+Remova os estados com a população menor que 4.000.000;
+Apague o dicionário de estados;
+Confira se o dicionário está vazio.
+ */
+
+public class ExerciciosPropostos1 {
+    public static void main(String[] args) {
+
+    }
+}
